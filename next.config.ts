1ba8e8608c4093436ba5e441d8a62a7e451b5099
Umut <sumutyigitoglu@hotmail.com>
2025-05-28 00:17:53 +0300

    fix: prod env

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/portfolio',
+  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
   images: {
     unoptimized: true,
   },
